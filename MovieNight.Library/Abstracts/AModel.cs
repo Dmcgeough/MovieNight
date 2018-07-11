8325246a50ca09f42f1bde782a7d65815ea41a8f
Dmcgeough <danny.mcgeough@gmail.com>
2018-07-11 15:08:42 -0400

    Added in Testing

---
+++ MovieNight.Library/Abstracts/AModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieNight.Library.Abstracts
+{
+    public class AModel
+    {
+        public Guid Id
+        {
+            get;
+        }
+        public AModel()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
